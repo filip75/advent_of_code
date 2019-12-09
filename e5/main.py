ddd3dfc9a7efd6cd899a77f29afded7c14ea9171
Filip <filip.janowski@gmail.com>
2019-12-10 00:01:12 +0100

    9

--- e5/main.py
+++ e5/main.py
@@ -1,40 +1,50 @@
 from typing import List, Optional
 
+POSITION_MODE = '0'
+IMMEDIATE_MODE = '1'
+RELATIVE_MODE = '2'
+
 
 class IntCode:
     def __init__(self, memory: List[int]):
-        self.start_memory = memory[:]
-        self.memory = memory[:]
+        self.start_memory = memory[:] + [0] * 10000
+        self.memory = self.start_memory[:]
         self.pointer = 0
+        self.relative_base = 0
 
     def reset(self):
         self.memory = self.start_memory[:]
         self.pointer = 0
+        self.relative_base = 0
 
-    @staticmethod
-    def is_immediate(modes: str, position: int) -> bool:
-        return modes[-1 - position:-2 - position:-1] == '1'
+    def get_mode(self, position: int) -> str:
+        return str(self.memory[self.pointer])[:-2][-1 - position:-2 - position:-1]
 
     def get_value(self, position: int) -> int:
-        modes = str(self.memory[self.pointer])[:-2]
-        if self.is_immediate(modes, position):
-            return self.memory[self.pointer + 1 + position]
+        return self.memory[self.get_address(position)]
+
+    def get_address(self, position: int):
+        mode = self.get_mode(position)
+        if mode == IMMEDIATE_MODE:
+            return self.pointer + 1 + position
+        elif mode == RELATIVE_MODE:
+            return self.relative_base + self.memory[self.pointer + 1 + position]
         else:
-            return self.memory[self.memory[self.pointer + 1 + position]]
+            return self.memory[self.pointer + 1 + position]
 
     def run(self, inp: int) -> Optional[int]:
         while True:
             op_code = str(self.memory[self.pointer])
             instruction = int(op_code[-2:])
             if instruction == 1:
-                self.memory[self.memory[self.pointer + 3]] = self.get_value(0) + self.get_value(1)
+                self.memory[self.get_address(2)] = self.get_value(0) + self.get_value(1)
                 self.pointer += 4
             elif instruction == 2:
-                self.memory[self.memory[self.pointer + 3]] = self.get_value(0) * self.get_value(1)
+                self.memory[self.get_address(2)] = self.get_value(0) * self.get_value(1)
                 self.pointer += 4
             elif instruction == 3:
                 if inp is not None:
-                    self.memory[self.memory[self.pointer + 1]] = inp
+                    self.memory[self.get_address(0)] = inp
                     inp = None
                     self.pointer += 2
                 else:
@@ -55,16 +65,19 @@ class IntCode:
                     self.pointer += 3
             elif instruction == 7:
                 if self.get_value(0) < self.get_value(1):
-                    self.memory[self.memory[self.pointer + 3]] = 1
+                    self.memory[self.get_address(2)] = 1
                 else:
-                    self.memory[self.memory[self.pointer + 3]] = 0
+                    self.memory[self.get_address(2)] = 0
                 self.pointer += 4
             elif instruction == 8:
                 if self.get_value(0) == self.get_value(1):
-                    self.memory[self.memory[self.pointer + 3]] = 1
+                    self.memory[self.get_address(2)] = 1
                 else:
-                    self.memory[self.memory[self.pointer + 3]] = 0
+                    self.memory[self.get_address(2)] = 0
                 self.pointer += 4
+            elif instruction == 9:
+                self.relative_base += self.get_value(0)
+                self.pointer += 2
             elif instruction == 99:
                 self.pointer += 1
                 return None
---
+++ e9/main.py
@@ -0,0 +1,10 @@
+from e5.main import IntCode
+
+if __name__ == '__main__':
+    with open("input.txt") as file:
+        code = [int(x) for x in file.readline().split(',')]
+
+    ic = IntCode(code)
+    print(ic.run(1))
+    ic.reset()
+    print(ic.run(2))
