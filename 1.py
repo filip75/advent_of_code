928e1e8b1b36181bfef0bce92145aff5cdd5c2d2
Filip <filip.janowski@gmail.com>
2020-12-01 18:52:03 +0100

    day 1

--- 1.py
+++ 1.py
@@ -3,9 +3,15 @@ from itertools import combinations
 with open("1.input") as file:
     numbers = [int(n) for n in file.readlines()]
 
-for combination in combinations(numbers, 3):
-    if sum(combination) == 2020:
-        r = 1
-        for n in combination:
-            r *= n
-        print(r)
+
+def find(numbers, count):
+    for combination in combinations(numbers, count):
+        if sum(combination) == 2020:
+            r = 1
+            for n in combination:
+                r *= n
+            return r
+
+
+print(find(numbers, 2))
+print(find(numbers, 3))
